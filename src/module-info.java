6f8078bf018c425cd9dfbf1d8a756b5056766233
Liza Dudyk <55206822+lizooo@users.noreply.github.com>
2020-01-23 04:04:05 +0200

    Added files for the lab no.1 (variant 7)

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author lizad
+ *
+ */
+module ua.lviv.iot {
+}
---
+++ src/ua/lviv/iot/firstLab/HotelMaker.java
@@ -0,0 +1,47 @@
+package ua.lviv.iot.firstLab;
+
+@Deprecated
+public class HotelMaker {
+
+	public static void main(String[] args) {
+
+		Hotel leopolis = new Hotel(2180, "Leopolis", 57, 1225.0);
+		Hotel grand = new Hotel();
+		Hotel hilton = new Hotel(12720, "Hilton", 190, 9650.0, true, "Tarasa Shevchenko", "Kyiv");
+
+		System.out.println(leopolis);
+		System.out.println(grand);
+		System.out.println(hilton);
+		System.out.println("------------------------------------------------");
+		System.out.println("Default value of static field:");
+
+		grand.PrintMinNumberOfRewiewsOnTripAdvisor();
+		Hotel.PrintStaticMinNumberOfRewiewsOnTripAdvisor();
+
+		Hotel.setMinNumberOfRewiewsOnTripAdvisor(15);
+
+		System.out.println("------------------------------------------------");
+		System.out.println("Value of static field after setting:");
+
+		grand.PrintMinNumberOfRewiewsOnTripAdvisor();
+		Hotel.PrintStaticMinNumberOfRewiewsOnTripAdvisor();
+		
+		int numberOfObjectsAccordingToTask = 4;
+		int counterOfCreatedObjects = 0;
+		
+		Hotel[] arrayOfHotels = new Hotel[numberOfObjectsAccordingToTask];
+		
+		while (counterOfCreatedObjects < numberOfObjectsAccordingToTask){
+		 arrayOfHotels[counterOfCreatedObjects] = new Hotel();
+		 counterOfCreatedObjects++;
+		}
+		
+		for (Hotel currentHotel : arrayOfHotels) {
+			System.out.println(currentHotel);
+		}
+		
+
+		
+	}
+
+}
